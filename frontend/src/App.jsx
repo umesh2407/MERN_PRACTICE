536f00ea639a825ecd6a7d60233ce7d89b23f77c
umesh2407 <choudharyumesh986@gmail.com>
2025-02-07 12:26:01 +0530

    added useState for showing top rated cards

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,18 +1,18 @@
-import React from 'react'
-import Header from './components/Header'
-import Body from './components/Body'
-import Footer from './components/Footer'
+import React from "react";
+import Header from "./components/Header";
+import Body from "./components/Body";
+import Footer from "./components/Footer";
 
 const App = () => {
   return (
     <>
-    <div className='bg-black text-white w-full h-full'>
-<Header/>
-<Body/>
-<Footer/>
-</div>
+      <div className="bg-black text-white w-full h-full">
+        <Header />
+        <Body />
+        <Footer />
+      </div>
     </>
-  )
-}
+  );
+};
 
-export default App
+export default App;
