d9a855087e266a61e03e56361883048b90b327c5
umesh2407 <choudharyumesh986@gmail.com>
2025-02-10 12:25:18 +0530

    added search functionality

--- frontend/src/components/Body.jsx
+++ frontend/src/components/Body.jsx
@@ -1,33 +1,51 @@
-import React, { useState } from "react";
-import ResturantCard from "./ResturantCard"; 
+import React, { useEffect, useState } from "react";
+import ResturantCard from "./ResturantCard";
 import resList from "../utils/mockData";
 
 const Body = () => {
-  const [reslist, setReslist] = useState(resList)
-  const topRated = () =>{
-    const filterdList =  reslist.filter((rest) => rest.rating > 4.5)
-   setReslist(filterdList);
-  }
+  const [reslist, setReslist] = useState(resList);
+  const [filteredRestlist, setFilteredRestlist] = useState(reslist);
+  const [inputsearch, setInputsearch] = useState("");
 
+
+  const topRated = () => {
+    const filterdList = reslist.filter((rest) => rest.rating > 4.5);
+    setFilteredRestlist(filterdList);
+  };
+
+  const searchText = () => {
+    const filterdList = reslist.filter((rest) => rest.name.includes(inputsearch));
+    setFilteredRestlist(filterdList);
+  };
 
   return (
     <div>
-      <div>
-        <input
-          className="border border-black rounded-lg p-1 px-8 bg-slate-900 text-white m-5 mt-5 mb-5 items-center"
-          placeholder="Search"
-        ></input>
-        <button className="border border-white rounded-lg p-2 bg-slate-800"
-        onClick={topRated}
+      <div className="flex gap-10  mt-5 mb-5 m-5">
+        <div>
+          <input
+            className="border border-black rounded-lg p-1 px-8 bg-slate-900 text-white items-center"
+            placeholder="Search"
+            value={inputsearch}
+            onChange={(e) => setInputsearch(e.target.value)}
+          ></input>
+          <button
+            className="border border-gray-500 ml-5 rounded-sm p-2 bg-slate-800"
+            onClick={searchText}
+          >
+            Search
+          </button>
+        </div>
+        <button
+          className="border border-gray-500 rounded-sm  bg-slate-800 p-2"
+          onClick={topRated}
         >
-        Top Rated
+          Top Rated
         </button>
       </div>
       <div className="grid grid-cols-5 items-center ">
-        {reslist.map((rest) => (
-        
+        {filteredRestlist.map((rest) => (
           <ResturantCard key={rest.id} resObj={rest} />
-          ))}
+        ))}
       </div>
     </div>
   );
