ea5a1ae29b625e582942227257218fbf6928bea8
umesh2407 <choudharyumesh986@gmail.com>
2025-02-17 13:40:43 +0530

    added set state

--- frontend/src/pages/ContactUs.jsx
+++ frontend/src/pages/ContactUs.jsx
@@ -12,11 +12,19 @@ class ContactUs extends React.Component {
     return (
       <>
         <div className="text-center m-10 p-10">
-        {this.state.count}
-        {" "}    
-            Contact US
-            
-        </div>;
+          <button
+            onClick={() => {
+              this.setState({
+                count: this.state.count + 1,
+              });
+            }}
+            className="bg-slate-500 border rounded-lg p-2 mr-4"
+          >
+            Increase Count
+          </button>
+          Contact US {this.state.count}
+        </div>
+        ;
       </>
     );
   }
