68418533cc52ef9b0f3f8a920b62bf94f6d7c512
umesh2407 <choudharyumesh986@gmail.com>
2025-02-20 13:29:37 +0530

    added lazy loading in TAB form component

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { lazy, Suspense } from "react";
 import { createBrowserRouter, RouterProvider, Outlet } from "react-router-dom";
 import Home from "./pages/Home";
 import AboutUs from "./pages/AboutUs";
@@ -6,14 +6,17 @@ import Header from "./components/Header";
 import Footer from "./components/Footer";
 import ResturantMenu from "./pages/ResturantMenu";
 import ContactUs from "./pages/ContactUs";
-import TabForm from "./pages/TabForm";
+// import TabForm from "./pages/TabForm";
+
+//using lazy loading logic
+const TabForm = lazy(() => import("./pages/TabForm"));
 
 const Layout = () => {
   return (
     <div>
-      <Header/>
+      <Header />
       <Outlet />
-      <Footer/>
+      <Footer />
     </div>
   );
 };
@@ -21,13 +24,20 @@ const Layout = () => {
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <Layout />, 
+    element: <Layout />,
     children: [
       { path: "", element: <Home /> },
       { path: "aboutus", element: <AboutUs /> },
-      {path:"contact", element:<ContactUs/>},
-      {path:"form", element:<TabForm/>},
-      { path: "restuarant/:resId", element: <ResturantMenu/> },
+      { path: "contact", element: <ContactUs /> },
+      {
+        path: "form",
+        element: (
+          <Suspense fallback={<h1>Loading.......</h1>}>
+            <TabForm />
+          </Suspense>
+        ),
+      },
+      { path: "restuarant/:resId", element: <ResturantMenu /> },
     ],
   },
 ]);
